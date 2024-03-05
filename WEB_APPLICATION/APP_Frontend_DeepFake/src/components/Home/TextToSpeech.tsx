fa8c4124a18e5abefde6ec3534eedd8237a29d9f
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-03-06 01:54:47 +0500

    Speech Systhsis

--- WEB_APPLICATION/APP_Frontend_DeepFake/src/components/Home/TextToSpeech.tsx
+++ WEB_APPLICATION/APP_Frontend_DeepFake/src/components/Home/TextToSpeech.tsx
@@ -1,7 +1,31 @@
-// import React from 'react'
+import React from 'react'
 import { Select, Option, Button } from "@material-tailwind/react";
 
 function TextToSpeech() {
+  const speechSyn = new SpeechSynthesisUtterance();
+
+  const [text, setText] = React.useState('')
+  // const [voices, setVoices] = React.useState<SpeechSynthesisVoice[]>([])
+  
+  // const populateVoices = React.useCallback( ()=>{
+  //    setVoices(speechSyn.getVoices())
+  // }, [])
+
+  // React.useEffect(()=>{
+  //     populateVoices()
+  //   if (speechSyn.onvoiceschanged !== undefined)
+  //      speechSyn.onvoiceschanged = populateVoices
+
+  //   console.log(voices.length)
+  // }, [populateVoices]) 
+
+  
+  
+  function textToSpeechFeature(){
+    speechSyn.text = text
+    window.speechSynthesis.speak(speechSyn)
+  }
+
   return (
 
     <div className="w-full  my-14 ">
@@ -13,7 +37,11 @@ function TextToSpeech() {
             {/* Left Side */}
             <div id="TextArea" className="w-full md:w-[50%] box-border p-5 text-start">
                <label htmlFor="TextBox" className="">Max Words Allowed 1000</label>
-               <textarea id="TextBox" typeof="text" rows={9} placeholder="Enter your Mesage here" className="rounded text-base text-gray-800 w-full  placeholder:text-base p-3 shadow-sm shadow-indigo-800" />
+               <textarea id="TextBox" typeof="text" rows={9} placeholder="Enter your Mesage here" className="rounded text-base text-gray-800 w-full  placeholder:text-base p-3 shadow-sm shadow-indigo-800"
+               value={text}
+               onChange={ (e) => { setText(e.target.value) } }
+               
+               />
             </div>
 
             {/* Right Side */}
@@ -22,19 +50,27 @@ function TextToSpeech() {
                 <h1 className="mb-1">Select Voice Over</h1>
                 <Select color="purple" label="Select Version" placeholder={''} className="bg-white text-black">
                     <Option>Dr Wajahat Qazi</Option>
-                    <Option>Muhammad Talha</Option>
+                    <Option>Muhammad Talha</Option> 
+                    {/* <Option>{voices[0].name}</Option> */}
+                    {/* {
+                      voices.map( (voice)=>{
+                        <Option>{voice.name}</Option>
+                      })
+                    } */}
                 </Select>
              </div>
 
              <div className="hidden md:block  w-72 lg:w-96 ">
                 <h1>Language</h1>
-                <Select label="Select Version" placeholder={''} value="English" disabled>
+                <Select label="Select Version" placeholder={''} value="English" disabled >
                     <Option>English</Option>
                 </Select>
              </div>
 
              <div className="mt-7">
-                <Button variant="filled" placeholder={''} className="bg-indigo-800  w-72 lg:w-96 ">Convert to Speech</Button>
+                <Button variant="filled" placeholder={''} className="bg-indigo-800  w-72 lg:w-96 " 
+                onClick={textToSpeechFeature}>
+                  Convert to Speech</Button>
              </div>
             </div>
       </div>
